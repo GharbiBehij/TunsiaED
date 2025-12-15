5fc2564db8b3dd0d59ea2fe48df5214eb3d4a3ed
BehijGharbi <behijghrabi@gmail.com>
2025-12-15 08:48:57 +0100

    youp

--- bff/src/Modules/User/api/controllers/User.controller.js
+++ bff/src/Modules/User/api/controllers/User.controller.js
@@ -3,7 +3,7 @@ import { userService } from '../../service/User.service.js';
 import { OnboardUserSchema, UpdateUserSchema } from '../../Validators/User.schema.js';
 import { userRepository } from '../../repository/User.repository.js';
 import { FirebaseAuthAdapter } from '../../../../adapters/firebaseAdapter.js';
-import { mapFirebaseUserToSafeUser } from '../../../utils/FirebaseUserMapper.js';
+import { mapFirebaseUserToSafeUser } from '../../../../utils/FirebaseUserMapper.js';
 
 // Create new user profile during registration
 export const onboardUser = async (req, res) => {
